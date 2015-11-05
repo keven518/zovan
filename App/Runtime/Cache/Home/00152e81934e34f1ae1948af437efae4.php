2907608d52958d529b127977c0ab70a2692de282
kewen <957314724@qq.com>
2015-11-05 17:24:00 +0800

    tj

--- App/Runtime/Cache/Home/00152e81934e34f1ae1948af437efae4.php
+++ App/Runtime/Cache/Home/00152e81934e34f1ae1948af437efae4.php
@@ -5,10 +5,58 @@
     <title>账单系统</title>
     <link href="__PUBLIC__/css/ionic/ionic.min.css" rel="stylesheet">
     <script src="__PUBLIC__/js/ionic.bundle.min.js"></script>
+    <script src="__PUBLIC__/js_date/jquery.1.7.2.min.js"></script>
+        <script src="__PUBLIC__/js_date/mobiscroll_002.js" type="text/javascript"></script>
+      <script src="__PUBLIC__/js_date/mobiscroll_004.js" type="text/javascript"></script>
+      <link href="__PUBLIC__/css_date/mobiscroll_002.css" rel="stylesheet" type="text/css">
+      <link href="__PUBLIC__/css_date/mobiscroll.css" rel="stylesheet" type="text/css">
+      <script src="__PUBLIC__/js_date/mobiscroll.js" type="text/javascript"></script>
+      <script src="__PUBLIC__/js_date/mobiscroll_003.js" type="text/javascript"></script>
+      <script src="__PUBLIC__/js_date/mobiscroll_005.js" type="text/javascript"></script>
+      <link href="__PUBLIC__/css_date/mobiscroll_003.css" rel="stylesheet" type="text/css">
 	<title>Document</title>
 </head>
 <style>
 body {
+    padding: 0;
+    margin: 0;
+    font-family: arial, verdana, sans-serif;
+    font-size: 12px;
+    background: #ddd;
+}
+input, select {
+    width: 100%;
+    padding: 5px;
+    margin: 5px 0;
+    border: 1px solid #aaa;
+    box-sizing: border-box;
+    border-radius: 5px;
+    -moz-box-sizing: border-box;
+    -webkit-box-sizing: border-box;
+    -webkit-border-radius: 5px;
+}
+.header {
+    border: 1px solid #333;
+    background: #111;
+    color: white;
+    font-weight: bold;
+    text-shadow: 0 -1px 1px black;
+    background-image: linear-gradient(#3C3C3C,#111);
+    background-image: -webkit-gradient(linear,left top,left bottom,from(#3C3C3C),to(#111));
+    background-image: -moz-linear-gradient(#3C3C3C,#111);
+}
+.header h1 {
+    text-align: center;
+    font-size: 16px;
+    margin: .6em 0;
+    padding: 0;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+}
+.content {
+    padding: 15px;
+    background: #fff;
 }
 .item-select select.kv_select {
     -webkit-appearance: none;
@@ -37,7 +85,7 @@ body {
 </div>
 <form method='post' id="form_do" name="form_do" action="<?php echo U(GROUP_NAME. '/Bill/add');?>">
 <div class="content has-header" ng-app="" ng-init="quantity=1;price=5">
-  <div class="list">
+<div class="list">
     <label class="item item-input">
       <span class="input-label">名称：</span>
       <input type="text" name='name'>
@@ -56,7 +104,7 @@ body {
     </label>
     <label class="item item-input">
       <span class="input-label">购买时间：</span>
-      <input type="text"  name='buytime'>
+      <input type="text"  name='buytime' id="appDate">
     </label>
 
 	<div class="item item-input item-select">
@@ -95,19 +143,34 @@ body {
 
 
 
+
     
     <script src="__PUBLIC__/js/angular.min.js"></script>
-    <script type="text/javascript">
-	/*var myModele = angular.module('myMode',[]);
-	
-	myModele.directive('hello',function(){
-	     return{
-			 restrict:'B',
-			 template:'<div class="red">hello 2131313<strong>你好</strong>everyone</div>',
-			 replace:true
-		 }	
-	})*/
-    
-    </script>
+      <script type="text/javascript">
+            $(function () {
+          var currYear = (new Date()).getFullYear();  
+          var opt={};
+          opt.date = {preset : 'date'};
+          opt.datetime = {preset : 'datetime'};
+          opt.time = {preset : 'time'};
+          opt.default = {
+            theme: 'android-ics light', //皮肤样式
+                display: 'modal', //显示方式 
+                mode: 'scroller', //日期选择模式
+            dateFormat: 'yyyy-mm-dd',
+            lang: 'zh',
+            showNow: true,
+            nowText: "今天",
+                startYear: currYear - 10, //开始年份
+                endYear: currYear + 10 //结束年份
+          };
+
+            $("#appDate").mobiscroll($.extend(opt['date'], opt['default']));
+            var optDateTime = $.extend(opt['datetime'], opt['default']);
+            var optTime = $.extend(opt['time'], opt['default']);
+            $("#appDateTime").mobiscroll(optDateTime).datetime(optDateTime);
+            $("#appTime").mobiscroll(optTime).time(optTime);
+            });
+        </script>
 </body>
 </html>
